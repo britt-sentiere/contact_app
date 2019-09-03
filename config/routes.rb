432ade5458ddf09e36824009d769321afe361912
brittani <britt.sentiere@gmail.com>
2019-09-03 12:33:20 -0500

    end of day

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/first_contact_url' => 'contacts#first_contact_action'
-    get '/all_contacts_url' => 'contacts#all_contacts_action'
+    get '/first_contact' => 'contacts#index'
+    post '/first_contact' => 'contacts#create'
+    get '/all_contacts/:id' => 'contacts#show'
+    patch 'all_contacts/:id' => 'contacts#update'
+    delete 'all_contacts/:id' => 'contacts#destroy'
+
  end
 end
